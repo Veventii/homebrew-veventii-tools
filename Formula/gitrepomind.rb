e1a1ecdcf7788a4ca8fbe9473518a7c303284004
Milindu Kumarage <milindu.kumarage@veventii.com>
2026-05-30 21:25:09 +0800

    Release binary formula and assets for gitrepomind v0.1.0

--- Formula/gitrepomind.rb
+++ Formula/gitrepomind.rb
@@ -1,14 +1,21 @@
 class Gitrepomind < Formula
   desc "Predict and find related files in your Git history using logical coupling"
   homepage "https://github.com/Veventii/GitRepoMind"
-  url "https://static.crates.io/crates/gitrepomind/gitrepomind-0.1.0.crate"
-  sha256 "70a9a709e1199aa0fb65cbb87c2c0363b4c413009517ad586bbc62697987876c"
+  version "0.1.0"
   license "MIT"
 
-  depends_on "rust" => :build
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/Veventii/homebrew-veventii-tools/releases/download/v0.1.0/gitrepomind-v0.1.0-macos-arm64.tar.gz"
+      sha256 "7a9969a8ab1bb17cef1b0475635e60ac0bdc9757c8acbbd30d5f78ce08e37d9f"
+    else
+      url "https://github.com/Veventii/homebrew-veventii-tools/releases/download/v0.1.0/gitrepomind-v0.1.0-macos-x86_64.tar.gz"
+      sha256 "5abe252f733f2ccdd1aa5a8d5c0ff7180dea915724dcedc7429986887762e2da"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "gitrepomind"
   end
 
   test do
